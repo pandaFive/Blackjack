ccc6928f1345cc948fdb6cb46dce04d6b73e49ec
propan <ldspdk5v@gmail.com>
2023-10-23 12:11:19 +0000

    feature #11 Playerクラスの実装

---
+++ part_class/person/player.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require_relative "../person.rb"
+
+class Player < Person
+  def initialize
+    super("あなた")
+  end
+
+  def decide_action
+    puts "#{@name}の現在の得点は#{@hands.calculate_score}です。カードを引きますか？：(y/n)"
+    decide = gets.chomp
+
+    while decide != "y" && decide != "n"
+      puts "yかnを入力してください。："
+      decide = gets.chomp
+    end
+
+    if decide == "y"
+      "Hit"
+    else
+      "Stand"
+    end
+  end
+
+  def receive_card(card)
+    @hands.add_card(card)
+
+    puts "#{@name}が引いたカードは#{card}です"
+
+    score = @hands.calculate_score
+
+    if score > 21
+      @state = "bust"
+
+      puts "#{@name} was busted"
+    end
+  end
+end
